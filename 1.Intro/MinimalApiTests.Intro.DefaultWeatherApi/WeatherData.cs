facc5de128b7a64a3bc5f26691ad0d3157b5c458
Dzmitry Yasenka <dzmitry.yasenka@gmail.com>
2022-05-13 11:20:31 +0100

    Default Weather API

---
+++ 1.Intro/MinimalApiTests.Intro.DefaultWeatherApi/WeatherData.cs
@@ -0,0 +1,8 @@
+﻿namespace MinimalApiTests.Intro.DefaultWeatherApi;
+
+public class WeatherData
+{
+    public static readonly string[] Summaries = {
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    };
+}
