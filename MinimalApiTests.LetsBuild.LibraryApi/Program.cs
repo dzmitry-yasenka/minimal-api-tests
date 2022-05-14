fb4f4cf31a5c7a6f9a9b5dab1e2837c239630083
Dzmitry Yasenka <dzmitry.yasenka@gmail.com>
2022-05-14 20:29:05 +0100

    Create a book functionality

--- MinimalApiTests.LetsBuild.LibraryApi/Program.cs
+++ MinimalApiTests.LetsBuild.LibraryApi/Program.cs
@@ -1,4 +1,6 @@
 using MinimalApiTests.LetsBuild.LibraryApi.Data;
+using MinimalApiTests.LetsBuild.LibraryApi.Models;
+using MinimalApiTests.LetsBuild.LibraryApi.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -7,6 +9,7 @@ builder.Services.AddSwaggerGen();
 builder.Services.AddSingleton<IDbConnectionFactory>(_ =>
     new SqliteConnectionFactory(builder.Configuration.GetValue<string>("Database:ConnectionString")));
 builder.Services.AddSingleton<DatabaseInitializer>();
+builder.Services.AddSingleton<IBookService, BookService>();
 
 var app = builder.Build();
 
@@ -16,5 +19,19 @@ app.UseSwaggerUI();
 // DB initialization here
 var databaseInitializer = app.Services.GetRequiredService<DatabaseInitializer>();
 await databaseInitializer.InitializeAsync();
+
+app.MapPost("books", async (Book book, IBookService bookService) =>
+{
+    var created = await bookService.CreateAsync(book);
+    if (!created)
+    {
+        return Results.BadRequest(new
+        {
+            errorMessage = "A book with this ISBN-13 is already exists"
+        });
+    }
+
+    return Results.Created($"/books/{book.Isbn}", book);
+});
 
 app.Run();
---
+++ MinimalApiTests.LetsBuild.LibraryApi/Services/BookService.cs
@@ -0,0 +1,56 @@
+﻿using Dapper;
+using MinimalApiTests.LetsBuild.LibraryApi.Data;
+using MinimalApiTests.LetsBuild.LibraryApi.Models;
+
+namespace MinimalApiTests.LetsBuild.LibraryApi.Services;
+
+public class BookService : IBookService
+{
+    private readonly IDbConnectionFactory _dbConnectionFactory;
+
+    public BookService(IDbConnectionFactory dbConnectionFactory)
+    {
+        _dbConnectionFactory = dbConnectionFactory;
+    }
+
+    public async Task<bool> CreateAsync(Book book)
+    {
+        var existingBook = await GetByIsbnAsync(book.Isbn);
+        if (existingBook is not null)
+        {
+            return false;
+        }
+
+        var connection = await _dbConnectionFactory.CreateConnectionAsync();
+        var result = await connection.ExecuteAsync(
+            @"INSERT INTO Books (Isbn, Title, Author, ShortDescription, PageCount, ReleaseDate) 
+                                VALUES (@Isbn, @Title, @Author, @ShortDescription, @PageCount, @ReleaseDate)", book);
+
+        return result > 0;
+    }
+
+    public Task<Book?> GetByIsbnAsync(string isbn)
+    {
+        return Task.FromResult<Book?>(null);
+    }
+
+    public Task<IEnumerable<Book>> GetAllAsync()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<IEnumerable<Book>> SearchByTitleAsync(string searchTerm)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<bool> UpdateAsync(Book book)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<bool> DeleteAsync(string isbn)
+    {
+        throw new NotImplementedException();
+    }
+}
