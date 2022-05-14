de6a64adafea013c59e144f54a7067053f9a7d04
Dzmitry Yasenka <dzmitry.yasenka@gmail.com>
2022-05-14 22:00:02 +0100

    Validation for creation of books

--- MinimalApiTests.LetsBuild.LibraryApi/Program.cs
+++ MinimalApiTests.LetsBuild.LibraryApi/Program.cs
@@ -1,3 +1,5 @@
+using FluentValidation;
+using FluentValidation.Results;
 using MinimalApiTests.LetsBuild.LibraryApi.Data;
 using MinimalApiTests.LetsBuild.LibraryApi.Models;
 using MinimalApiTests.LetsBuild.LibraryApi.Services;
@@ -10,28 +12,36 @@ builder.Services.AddSingleton<IDbConnectionFactory>(_ =>
     new SqliteConnectionFactory(builder.Configuration.GetValue<string>("Database:ConnectionString")));
 builder.Services.AddSingleton<DatabaseInitializer>();
 builder.Services.AddSingleton<IBookService, BookService>();
+builder.Services.AddValidatorsFromAssemblyContaining<Program>();
 
 var app = builder.Build();
 
 app.UseSwagger();
 app.UseSwaggerUI();
 
-// DB initialization here
-var databaseInitializer = app.Services.GetRequiredService<DatabaseInitializer>();
-await databaseInitializer.InitializeAsync();
-
-app.MapPost("books", async (Book book, IBookService bookService) =>
+app.MapPost("books", async (Book book, IBookService bookService, IValidator<Book> validator) =>
 {
+    var validationResult = await validator.ValidateAsync(book);
+
+    if (!validationResult.IsValid)
+    {
+        return Results.BadRequest(validationResult.Errors);
+    }
+    
     var created = await bookService.CreateAsync(book);
     if (!created)
     {
-        return Results.BadRequest(new
+        return Results.BadRequest(new List<ValidationFailure>
         {
-            errorMessage = "A book with this ISBN-13 is already exists"
+            new ValidationFailure("Isbn", "A book with this ISBN-13 already exists")
         });
     }
 
     return Results.Created($"/books/{book.Isbn}", book);
 });
+
+// DB initialization here
+var databaseInitializer = app.Services.GetRequiredService<DatabaseInitializer>();
+await databaseInitializer.InitializeAsync();
 
 app.Run();
